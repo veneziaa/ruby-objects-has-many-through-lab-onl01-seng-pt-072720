6d1d27480e0446caa59d9a70cf9aa0c77bfef520
Venice Vargas <venicevargas1@live.com>
2020-09-20 15:45:34 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,21 @@
+class Genre
+
+  attr_reader :name, :genre
+  attr_accessor :song
+
+  def initialize(name)
+    @name = name
+  end
+
+  def songs
+    Song.all.select do |song|
+      song.genre == self
+    end
+  end
+
+  def artists
+    Song.all.map do |song|
+      song.artist
+    end
+  end
+end
